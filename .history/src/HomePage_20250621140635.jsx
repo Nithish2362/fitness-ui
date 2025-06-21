4b0633db13fb1f9f2a6fcbac6dbf1c21be228980
Nithish2362 <kumarnithish941@gmail.com>
2025-06-21 14:10:08 +0530

    added goal new  from new h2

---
+++ .history/src/HomePage_20250621140635.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to the Fitness App</h1>
+      <p>Get fit, stay healthy!</p>
+
+      <div className="features">
+        <p>YOUR BMR & BMI FROM UR SUBMITTED FORM</p>
+        <h2>BMR :</h2>
+        <h2>BMI:</h2>
+        <h2> new added</h2>
+        <h2>YOUR GOALNew2:</h2>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
