431645aa741a87d086f68f7911692fd9e8875e2e
Nithish2362 <kumarnithish941@gmail.com>
2025-06-21 14:01:25 +0530

    added goal new  from new

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
         <p>YOUR BMR & BMI FROM UR SUBMITTED FORM</p>
         <h2>BMR :</h2>
         <h2>BMI:</h2>
-        <h2>YOUR GOAL:</h2>
+        <h2>YOUR GOAL from new :</h2>
       </div>
     </div>
   );
